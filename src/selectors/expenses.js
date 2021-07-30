ee5c24e041d5450fc1ed4f91993f0bcff6d9626e
Jean <81425194+jenchen-dev@users.noreply.github.com>
2021-07-30 22:05:01 +0800

    update

--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -1,4 +1,4 @@
-const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
+const selectExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
     return expenses.filter((expense) => {
         const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
         const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
@@ -14,4 +14,4 @@ const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
     });
 };
 
-export default getVisibleExpenses;
+export default selectExpenses;
