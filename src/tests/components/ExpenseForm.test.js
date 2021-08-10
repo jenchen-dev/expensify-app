026b739159933a0f2142a611f5cf49d0337878c9
Jean <81425194+jenchen-dev@users.noreply.github.com>
2021-08-10 21:48:09 +0800

    add tests for components

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import moment from 'moment';
 import ExpenseForm from '../../components/ExpenseForm';
+import { SingleDatePicker } from 'react-dates';
 import expenses from '../fixtures/expenses';
 
 test('should render ExpenseForm correctly', () => {
@@ -38,4 +40,51 @@ test('should set note on textarea change', () => {
         target: { value }
     });
     expect(wrapper.state('note')).toBe(value);
+});
+
+test('should set amount if valid input', () => {
+    const value = '23.50';
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('input').at(1).simulate('change', {
+        target: { value }
+    });
+    expect(wrapper.state('amount')).toBe(value);
+});
+
+test('should not set amount if invalid input', () => {
+    const value = '12.1224';
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('input').at(1).simulate('change', {
+        target: { value }
+    });
+    expect(wrapper.state('amount')).toBe('');
+});
+
+test('should call onSubmit props for valid form submission', () => {
+    const onSubmitSpy = jest.fn();
+    const wrapper = shallow(<ExpenseForm expense={expenses[0]} onSubmit={onSubmitSpy}/>);
+    wrapper.find('form').simulate('submit', {
+        preventDefault: () => {}
+    });
+    expect(wrapper.state('error')).toBe('');
+    expect(onSubmitSpy).toHaveBeenLastCalledWith({
+        description: expenses[0].description,
+        amount: expenses[0].amount,
+        note: expenses[0].note,
+        createdAt: expenses[0].createdAt
+    });
+});
+
+test('should set new date on date change', () => {
+    const now = moment();
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find(SingleDatePicker).prop('onDateChange')(now);
+    expect(wrapper.state('createdAt')).toEqual(now);
+});
+
+test('should set calenar focus on change', () => {
+    const focused = true;
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find(SingleDatePicker).prop('onFocusChange')({ focused });
+    expect(wrapper.state('calendarFocused')).toBe(focused);
 });
