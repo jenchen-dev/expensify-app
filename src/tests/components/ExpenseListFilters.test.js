cc37edcf199aab012477435141123da8fa03c70d
Jean <81425194+jenchen-dev@users.noreply.github.com>
2021-08-13 11:10:25 +0800

    add tests for components

--- src/tests/components/ExpenseListFilters.test.js
+++ src/tests/components/ExpenseListFilters.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import moment from 'moment';
 import { ExpenseListFilters } from '../../components/ExpenseListFilters';
 import { filters, altFilters } from '../fixtures/filters';
+import { DateRangePicker } from 'react-dates';
 
 let setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate, wrapper;
 
@@ -62,9 +64,15 @@ test('should sort by amount', () => {
 });
 
 test('should handle date changes', () => {
-
+    const startDate = moment(0).add(4, 'years');
+    const endDate = moment(0).add(5, 'years');
+    wrapper.find(DateRangePicker).prop('onDatesChange')({ startDate, endDate });
+    expect(setStartDate).toHaveBeenLastCalledWith(startDate);
+    expect(setEndDate).toHaveBeenLastCalledWith(endDate);
 });
 
 test('should handle date focus changes', () => {
-
+    const calendarFocused = 'endDate';
+    wrapper.find(DateRangePicker).prop('onFocusChange')(calendarFocused);
+    expect(wrapper.state('calendarFocused')).toBe(calendarFocused);
 });
