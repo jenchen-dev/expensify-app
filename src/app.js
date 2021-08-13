8ef698d47e4fc551e25318400dc4804a56dc948b
Jean <81425194+jenchen-dev@users.noreply.github.com>
2021-08-13 11:21:46 +0800

    remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -3,15 +3,11 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-
-const state = store.getState();
-getVisibleExpenses(state.expenses, state.filters);
 
 const jsx = (
     <Provider store={store}>
