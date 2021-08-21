8a5feaaa3f5c835c8f916d37bc8344b820345c67
Jean <81425194+jenchen-dev@users.noreply.github.com>
2021-08-21 15:10:25 +0800

    add expense-total reducer

---
+++ src/selectors/expenses-total.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const selectExpensesTotal = (expenses) => {
+    if (expenses.length === 0) {
+        return 0;
+    } else {
+        return expenses
+            .map((expenses) => expenses.amount)
+            .reduce((sum, value) => sum + value, 0);
+    }
+};
+
+export default selectExpensesTotal;
