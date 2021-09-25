84bfb6fdd777ba4879891812bcb1c9b28f1b7e8f
Jean <81425194+jenchen-dev@users.noreply.github.com>
2021-09-25 14:31:55 +0800

    add function for login and logout auth

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -16,5 +16,6 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+googleAuthProvider.setCustomParameters({ 'prompt': 'select_account' });
 
 export { firebase, googleAuthProvider, database as default };
