8124c6a8bf04a07f6891c1f369883921a396b4fb
Jean <81425194+jenchen-dev@users.noreply.github.com>
2021-09-26 20:34:10 +0800

    update private and public pages

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
-import Header from '../components/Header';
+import PublicRoute from './PublicRoute';
+import PrivateRoute from './PrivateRoute';
+
 import LoginPage from '../components/LoginPage';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
-import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
 
 export const history = createBrowserHistory();
@@ -14,13 +15,11 @@ export const history = createBrowserHistory();
 const AppRouter = () => (
     <Router history={history}>
         <div>
-            <Header />
             <Switch>
-                <Route exact path="/" component={LoginPage} />
-                <Route path="/dashboard" component={ExpenseDashboardPage} />
-                <Route path="/create" component={AddExpensePage} />
-                <Route path="/edit/:id" component={EditExpensePage} />
-                <Route path="/help" component={HelpPage} />
+                <PublicRoute exact path="/" component={LoginPage} />
+                <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
+                <PrivateRoute path="/create" component={AddExpensePage} />
+                <PrivateRoute path="/edit/:id" component={EditExpensePage} />
                 <Route component={NotFoundPage} />
             </Switch>
         </div>
