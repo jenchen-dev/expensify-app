1e05322b32ba4a09e7fce8aefed425ccf0e76aed
Jean <81425194+jenchen-dev@users.noreply.github.com>
2021-09-30 21:51:21 +0800

    add style in app

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -7,7 +7,7 @@ export const LoginPage = ({ startLogin }) => (
         <div className="box-layout__box">
             <h1 className="box-layout__title">Expensify App</h1>
             <p>It's time to get your expenses under control.</p>
-            <button className="login-button" onClick={startLogin}>Login with Google</button>
+            <button className="button" onClick={startLogin}>Login with Google</button>
         </div>
     </div>
 );
