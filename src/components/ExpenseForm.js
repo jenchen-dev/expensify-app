5cd562683fddb8d31a1cf188ebafa51c9f790060
Jean <81425194+jenchen-dev@users.noreply.github.com>
2021-10-01 23:42:31 +0800

    add css styles to app

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -40,7 +40,7 @@ export default class ExpenseForm extends React.Component {
     onSubmitExpenseForm = (e) => {
         e.preventDefault();
         if (!this.state.description || !this.state.amount) {
-            this.setState(() => ({ error: 'Please provide description and amount.' }));
+            this.setState(() => ({ error: '* Please provide both description and amount.' }));
         } else {
             this.setState(() => ({ error: '' }));
             this.props.onSubmit({
@@ -53,40 +53,44 @@ export default class ExpenseForm extends React.Component {
     };
     render() {
         return (
-            <div>
-                {this.state.error && <p>{this.state.error}</p>}
-                <form onSubmit={this.onSubmitExpenseForm}>
-                    <input
-                        type="text"
-                        placeholder="Description"
-                        autoFocus
-                        value={this.state.description}
-                        onChange={this.onDescriptionChange}
-                    />
-                    <input
-                        type="text"
-                        placeholder="Amount"
-                        value={this.state.amount}
-                        onChange={this.onAmountChange}
-                    />
-                    <SingleDatePicker
-                        date={this.state.createdAt}
-                        onDateChange={this.onDateChange}
-                        focused={this.state.calendarFocused}
-                        onFocusChange={this.onFocusChange}
-                        numberOfMonths={1}
-                        isOutsideRange={() => false}
-                        id="expense_date_picker"
-                    />
-                    <textarea
-                        placeholder="Add a note for your expense (optional)"
-                        value={this.state.note}
-                        onChange={this.onNoteChange}
-                    >
-                    </textarea>
-                    <button>{this.props.expense? 'Done Edit' : 'Add Expense' }</button>
-                </form>
-            </div>
+            <form className="form" onSubmit={this.onSubmitExpenseForm}>
+                {this.state.error && <p className="form__error">{this.state.error}</p>}
+                <input
+                    type="text"
+                    placeholder="Description"
+                    className={this.state.error && this.state.description === '' ? "text-input__error" : "text-input"}
+                    autoFocus
+                    value={this.state.description}
+                    onChange={this.onDescriptionChange}
+                />
+                <input
+                    type="text"
+                    placeholder="Amount"
+                    className={this.state.error && this.state.amount === '' ? "text-input__error" : "text-input"}
+                    value={this.state.amount}
+                    onChange={this.onAmountChange}
+                />
+                <SingleDatePicker
+                    date={this.state.createdAt}
+                    onDateChange={this.onDateChange}
+                    focused={this.state.calendarFocused}
+                    onFocusChange={this.onFocusChange}
+                    numberOfMonths={1}
+                    isOutsideRange={() => false}
+                    id="expense_date_picker"
+                    block
+                />
+                <textarea
+                    placeholder="Add a note for your expense (optional)"
+                    className="textarea"
+                    value={this.state.note}
+                    onChange={this.onNoteChange}
+                >
+                </textarea>
+                <div>
+                    <button className="button">{this.props.expense? 'Save Expense' : 'Add Expense' }</button>
+                </div>
+            </form>
         );
     };
 }
--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -30,13 +30,16 @@ export class ExpenseListFilters extends React.Component {
                 <div className="input-group">
                     <div className="input-group__item">
                         <input 
-                            type="text" 
-                            value={this.props.filters.text} 
+                            type="text"
+                            className="text-input"
+                            placeholder="Search expenses"
+                            value={this.props.filters.text}
                             onChange={this.onTextChange}
                         />
                     </div>
                     <div className="input-group__item">
-                        <select 
+                        <select
+                            className="select"
                             value={this.props.filters.sortBy} 
                             onChange={this.onSortChange}
                         >
